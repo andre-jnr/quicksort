bc5a3d03b64a784d9a097ae44bdfc5ecdf9b3181
Andre Junior <andrejunior.pessoal@gmail.com>
2024-04-27 21:43:36 -0400

    Metodos que retornam o big O do pior caso, e a quantidade de operações realizadas

--- main.py
+++ main.py
@@ -5,12 +5,23 @@ arquivo = Arquivo('dados.txt')
 arquivo.inicializarBaseDeDados()
 
 lista_original = arquivo.listarNomes()
-print(lista_original)
+# print(lista_original)
 
 lista_quicksort = Ordenacao.quicksort(lista_original)
 lista_por_selecao = Ordenacao.porSelecao(lista_original)
-lista_por_insercao = Ordenacao.porSelecao(lista_original)
+lista_por_insercao = Ordenacao.porInsercao(lista_original)
 
-print(lista_quicksort)
-print(lista_por_selecao)
-print(lista_por_insercao)
+print(f'Operações quicksort: {lista_quicksort['operacoes']}')
+bigO = Ordenacao.calcularBigO(len(lista_original), algoritmo='quicksort')
+print(f'BigO quicksort: {bigO}')
+print('')
+
+print(f'Operações seleção: {lista_por_selecao['operacoes']}')
+bigO = Ordenacao.calcularBigO(len(lista_original), algoritmo='por_selecao')
+print(f'BigO por seleção: {bigO}')
+print('')
+
+print(f'Operações de ordenação por inserção: {
+      lista_por_insercao['operacoes']}')
+bigO = Ordenacao.calcularBigO(len(lista_original), algoritmo='por_insercao')
+print(f'BigO por inserção: {bigO}')
--- models/Ordenacao.py
+++ models/Ordenacao.py
@@ -1,33 +1,68 @@
 class Ordenacao:
     @staticmethod
-    def quicksort(array):
+    def quicksort(array, steps=0):
         if len(array) < 2:
-            return array
+            steps += 1
+            return {'lista_ordenada': array, 'operacoes': steps}
         else:
             pivo = array[0]
             menores = [x for x in array[1:] if x <= pivo]
             maiores = [x for x in array[1:] if x > pivo]
-            return Ordenacao.quicksort(menores) + [pivo] + Ordenacao.quicksort(maiores)
+
+            resultado_menores = Ordenacao.quicksort(menores, steps)
+            resultado_maiores = Ordenacao.quicksort(
+                maiores, resultado_menores['operacoes'])
+
+            lista_ordenada = resultado_menores['lista_ordenada'] + \
+                [pivo] + resultado_maiores['lista_ordenada']
+            steps = resultado_maiores['operacoes'] + len(array)
+
+            return {'lista_ordenada': lista_ordenada, 'operacoes': steps}
 
     @staticmethod
     def porSelecao(array):
         array_copia = array.copy()
         nova_lista = []
+        steps = 0
 
         while array_copia:
+            steps += 1
             menor_valor = min(array_copia)
+            steps += len(array_copia)
             nova_lista.append(menor_valor)
             array_copia.remove(menor_valor)
 
-        return nova_lista
+        json = {'lista_ordenada': nova_lista,
+                'operacoes': steps}
+        return json
 
     @staticmethod
     def porInsercao(array):
+        steps = 0
         for i in range(1, len(array)):
+            steps += 1
             valor = array[i]
             j = i - 1
             while j >= 0 and array[j] > valor:
+                steps += 1
                 array[j + 1] = array[j]
                 j -= 1
             array[j + 1] = valor
-        return array
+        steps += 1
+
+        json = {'lista_ordenada': array,
+                'operacoes': steps}
+        return json
+
+    @staticmethod
+    def calcularBigO(tamanho_lista, algoritmo, caso='pior'):
+        from math import log2
+        if algoritmo == 'quicksort':
+            if caso == 'medio':
+                notacao_bigO = tamanho_lista * log2(tamanho_lista)
+                notacao_bigO = int(notacao_bigO)
+                return notacao_bigO
+            elif caso == 'pior':
+                return pow(tamanho_lista, 2)
+        elif algoritmo in ['por_selecao', 'por_insercao']:
+            return pow(tamanho_lista, 2)
